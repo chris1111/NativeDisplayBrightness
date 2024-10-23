9090edd132701111a861d8a0caa98a6fa53cb3dc
chris1111 <leblond1111@gmail.com>
2024-10-22 20:19:44 -0400

    Use Arrow Left and Right Beta

--- NativeDisplayBrightness/AppDelegate.m
+++ NativeDisplayBrightness/AppDelegate.m
@@ -102,7 +102,7 @@ CGEventRef keyboardCGEventCallback(CGEventTapProxy proxy,
 {
     [NSEvent addGlobalMonitorForEventsMatchingMask:NSEventMaskKeyDown | NSEventMaskKeyUp handler:^(NSEvent *_Nonnull event) {
         //NSLog(@"event!!");
-        if (event.keyCode == kVK_F1)
+        if (event.keyCode == kVK_LeftArrow)
         {
             if (event.type == NSEventTypeKeyDown)
             {
@@ -111,7 +111,7 @@ CGEventRef keyboardCGEventCallback(CGEventTapProxy proxy,
                 });
             }
         }
-        else if (event.keyCode == kVK_F2)
+        else if (event.keyCode == kVK_RightArrow)
         {
             if (event.type == NSEventTypeKeyDown)
             {
